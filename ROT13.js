6cbe1a1f21da1b33e2917031782fc4f16908eb60
sambeng1 <42613919+sambeng1@users.noreply.github.com>
2018-11-26 11:06:13 -0600

    Update ROT13.js

--- ROT13.js
+++ ROT13.js
@@ -11,34 +11,34 @@ Only letters from the latin/english alphabet should be shifted, like in the orig
 
 function rot13(message){
     
-    let ROT13Dictionary = {}
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    let alphabetUpper = alphabet.toUpperCase();
-
+  let ROT13Dictionary = {}
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let alphabetUpper = alphabet.toUpperCase();
+    
     for (let i = 0; i < alphabet.length; i++){
         let curLetter = alphabet[i];
         let curLetterUpper = alphabetUpper[i];
         let ROT13UpperLetter = alphabetUpper[i+13]
         let ROT13Letter = alphabet[i+13];
         if (ROT13Letter && ROT13UpperLetter){
-          ROT13Dictionary[curLetter] = ROT13Letter;
-          ROT13Dictionary[curLetterUpper] = ROT13UpperLetter
+        ROT13Dictionary[curLetter] = ROT13Letter;
+        ROT13Dictionary[curLetterUpper] = ROT13UpperLetter
         }
         if (ROT13Letter === undefined || ROT13UpperLetter === undefined){
-          ROT13Dictionary[curLetter] = alphabet[i-13]
-          ROT13Dictionary[curLetterUpper] = alphabetUpper[i-13];
+            ROT13Dictionary[curLetter] = alphabet[i-13]
+            ROT13Dictionary[curLetterUpper] = alphabetUpper[i-13];
         }
     }
-
-    let rotMessage = '';
-
+    
+  let rotMessage = '';
+    
     for (let i = 0; i < message.length; i++){
         let curChar = message[i];
         let rotCurChar = ROT13Dictionary[curChar];
         if (rotCurChar === undefined){
             rotMessage += curChar;
         }
-       else{rotMessage += rotCurChar;}
+       else{ rotMessage += rotCurChar;}
     }
     return rotMessage;
 }
